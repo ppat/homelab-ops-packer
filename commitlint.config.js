16495370283278224fe5e2c1ce71e37a6b334062
Peter Pathirana <peter@latent.io>
2024-06-15 23:59:55 +0000

    ci: first commit

---
+++ commitlint.config.js
@@ -0,0 +1,44 @@
+const validateBodyMaxLengthIgnoringDeps = async (parsedCommit) => {
+  const { maxLineLength } = await import('@commitlint/ensure');
+
+  const { type, scope, body } = parsedCommit
+  const isDepsCommit = type === 'chore' && scope === 'deps'
+
+  const bodyMaxLineLength = 120;
+
+  return [
+    isDepsCommit || !body || maxLineLength(body, bodyMaxLineLength),
+    `commit message body line length must not exceed ${bodyMaxLineLength}`,
+  ]
+}
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    // increase max line length for header
+    'header-max-length': [2, 'always', 120],
+
+    // disable max line length for footers
+    'footer-max-line-length': [0, 'always'],
+
+    // disable default 'body-max-line-length' rule and add custom rule for body-max-line-length
+    'body-max-line-length': [0],
+    'function-rules/body-max-line-length': [
+      2,
+      'always',
+      validateBodyMaxLengthIgnoringDeps
+    ],
+
+    // specify the allowed scopes
+    'scope-enum': [2, 'always',
+      [
+        '',
+        'deps'
+      ]
+    ],
+
+    // don't validate case of body
+    'body-case': [0, 'always']
+  }
+}
